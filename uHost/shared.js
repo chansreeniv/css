da4f366252ababea5e41b22c4d9fee57f9b1f88d
chansreeniv <chansreeniv@yahoo.com>
2021-02-10 07:30:40 +0530

    sass added

--- uHost/shared.js
+++ uHost/shared.js
@@ -4,6 +4,7 @@ var selectPlanButton = document.querySelectorAll(".plan button");
 var onClickNO = document.querySelector(".modal__action--negative");
 var toggleButton = document.querySelector(".toggle-button");
 var mobileNav = document.querySelector(".mobile-nav");
+var ctaButton = document.querySelector(".main-nav__item--cta")
 
 console.log(selectPlanButton);
 // console.dir(backdrop.style['background-image']);
@@ -14,7 +15,10 @@ for (var i = 0; i < selectPlanButton.length; i++) {
     // modal.style.display = "block";
     // backdrop.style.display = "block";
     modal.classList.add('open');
-    backdrop.classList.add('open');
+    backdrop.style.display = "block";
+    setTimeout(() => {
+      backdrop.classList.add('open');
+    }, 10);
   });
 }
 if (onClickNO) {
@@ -35,12 +39,29 @@ if (modal) {
     modal.classList.remove('open');
 }
 backdrop.classList.remove('open');
+setTimeout(() => {
+  backdrop.style.display = "none";
+}, 200);
 }
 
 toggleButton.addEventListener('click', () => {
     // mobileNav.style.display = 'block';
     // backdrop.style.display = 'block';
     mobileNav.classList.add('open');
-    backdrop.classList.add('open');
+    backdrop.style.display = "block";
+    setTimeout(() => {
+      backdrop.classList.add('open');
+    }, 10);
 })
 
+ctaButton.addEventListener('animationstart', (event) => {
+  console.log('Animation Started', event);
+});
+
+ctaButton.addEventListener('animationend', (event) => {
+  console.log('Animation Ended', event);
+});
+
+ctaButton.addEventListener('animationiteration', (event) => {
+  console.log('Animation iteration', event);
+});
